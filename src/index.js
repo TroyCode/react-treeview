303a5765be804521ef6129c89ff89ff2555b44ed
Troy Wu <pightmp@gmail.com>
2018-05-29 16:11:20 +0800

    feat: add service worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Uncontrolled from './uncontrolled';
 import Controlled from './controlled';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Uncontrolled />, document.getElementById('uncontrolled'));
 ReactDOM.render(<Controlled />, document.getElementById('controlled'));
+registerServiceWorker();
